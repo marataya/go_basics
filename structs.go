4c358101633960cba4043a3b0faf7a36b177c51f
Marat Ayaibergenov <marataya@gmail.com>
2023-07-27 03:16:02 +0600

    added structs

--- structs.go
+++ structs.go
@@ -1,1 +1,26 @@
 package main
+
+import "fmt"
+
+type Point struct {
+	X, Y int
+}
+
+type Circle struct {
+	Point
+	Radius int
+}
+
+type Wheel struct {
+	Circle
+	Spokes int
+}
+
+func (w Wheel) String() string {
+	return fmt.Sprintf("Center of wheel: %d, %d; No of spokes: %d", w.Circle.Point.X, w.Y, w.Spokes)
+}
+
+func main() {
+	w := Wheel{Circle{Point{X: 8, Y: 8}, 5}, 20}
+	fmt.Println(w)
+}
